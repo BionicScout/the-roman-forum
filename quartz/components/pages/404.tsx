4c93d4e38417922df00b29564843d70b3f3bc4de
Roman Higginson <theromanhigginson@gmail.com>
2026-05-03 19:16:12 -0400

    published: 2026-05-03 19:16:12

--- quartz/components/pages/404.tsx
+++ quartz/components/pages/404.tsx
@@ -1,18 +1,18 @@
 import { i18n } from "../../i18n"
 import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } from "../types"
 
-const NotFound: QuartzComponent = ({ i18n }: QuartzComponentProps) => {
+const NotFound: QuartzComponent = ({ cfg }: QuartzComponentProps) => {
+  // If baseUrl contains a pathname after the domain, use this as the home link
+  const url = new URL(`https://${cfg.baseUrl ?? "example.com"}`)
+  const baseDir = url.pathname
+
   return (
-    <div class="not-found">
-      <p>This page doesn't exist. Please return back to the main page.</p>
-      <a href="https://bionicscout.github.io/the-roman-forum" class="internal">Go back to home</a>
-    </div>
+    <article class="popover-hint">
+      <h1>404</h1>
+      <p>{i18n(cfg.locale).pages.error.notFound}</p>
+      <a href={baseDir}>{i18n(cfg.locale).pages.error.home}</a>
+    </article>
   )
 }
 
-export default (() => {
-  return {
-    content: NotFound,
-    title: "Page Not Found",
-  }
-}) satisfies QuartzComponentConstructor 
+export default (() => NotFound) satisfies QuartzComponentConstructor
