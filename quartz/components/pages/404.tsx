53dffc49c6f5df9b7d9c596993123ded8f66ba01
Roman Higginson <theromanhigginson@gmail.com>
2026-05-03 12:29:30 -0400

    published: 2026-05-03 12:29:30

--- quartz/components/pages/404.tsx
+++ quartz/components/pages/404.tsx
@@ -1,18 +1,18 @@
 import { i18n } from "../../i18n"
 import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } from "../types"
 
-const NotFound: QuartzComponent = ({ cfg }: QuartzComponentProps) => {
-  // If baseUrl contains a pathname after the domain, use this as the home link
-  const url = new URL(`https://${cfg.baseUrl ?? "example.com"}`)
-  const baseDir = url.pathname
-
+const NotFound: QuartzComponent = ({ i18n }: QuartzComponentProps) => {
   return (
-    <article class="popover-hint">
-      <h1>404</h1>
-      <p>{i18n(cfg.locale).pages.error.notFound}</p>
-      <a href={baseDir}>{i18n(cfg.locale).pages.error.home}</a>
-    </article>
+    <div class="not-found">
+      <p>This page doesn't exist. Please return back to the main page.</p>
+      <a href="/" class="internal">Go back to home</a>
+    </div>
   )
 }
 
-export default (() => NotFound) satisfies QuartzComponentConstructor
+export default (() => {
+  return {
+    content: NotFound,
+    title: "Page Not Found",
+  }
+}) satisfies QuartzComponentConstructor
