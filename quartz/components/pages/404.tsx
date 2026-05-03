86d491c73b551d7117d0069868e54e085f5fb0fc
Roman Higginson <theromanhigginson@gmail.com>
2026-05-03 19:50:16 -0400

    published: 2026-05-03 19:50:15

--- quartz/components/pages/404.tsx
+++ quartz/components/pages/404.tsx
@@ -1,16 +1,18 @@
 import { i18n } from "../../i18n"
 import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } from "../types"
+import { FullSlug, joinSegments } from "../../util/path"
 
 const NotFound: QuartzComponent = ({ cfg }: QuartzComponentProps) => {
   // If baseUrl contains a pathname after the domain, use this as the home link
   const url = new URL(`https://${cfg.baseUrl ?? "example.com"}`)
-  const baseDir = joinSegments(url.pathname, "the-roman-forum")
+  const baseDir = url.pathname 
 
   return (
     <article class="popover-hint">
       <h1>404</h1>
       <p>{i18n(cfg.locale).pages.error.notFound}</p>
-      <a href={baseDir}>{i18n(cfg.locale).pages.error.home}</a>
+      <a href={joinSegments(cfg.baseUrl ?? "", "the-roman-forum") as FullSlug}>
+  {i18n(cfg.locale).pages.error.home}</a>
     </article>
   )
 }
